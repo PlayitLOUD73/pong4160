1505e765bd7141b7fe3d3286df9480ae8d9b211e
PlayitLOUD73 <github@lukewelborn.com>
2023-02-08 15:47:08 -0500

    added boiler plate model

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import pygame, sys
 
 from view import View
 from controller import Controller
+from model import Model
     
 view = View()
 controller = Controller()
+model = Model()
 
 pygame.init()
 
