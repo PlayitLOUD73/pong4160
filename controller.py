9602812ea294e5dd498906a96e4b6098e8603d73
PlayitLOUD73 <github@lukewelborn.com>
2023-02-13 16:35:37 -0500

    Added event list creation

--- controller.py
+++ controller.py
@@ -5,7 +5,14 @@ import pygame, sys
 class Controller:
     
     def get_events(self):
+        exportKeys = []
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            exportKeys.append(pygame.K_UP)
+        elif keys[pygame.K_DOWN]:
+            exportKeys.append(pygame.K_DOWN)
+        return exportKeys
