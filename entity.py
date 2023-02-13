c0fa7d41a43311670b06b96bbe8c8b2867c079bc
PlayitLOUD73 <github@lukewelborn.com>
2023-02-13 16:34:29 -0500

    Added entity class

---
+++ entity.py
@@ -0,0 +1,9 @@
+import pygame, sys
+
+class Ent:
+
+    def __init__(self, name, kind, color, shape):
+        self.name = name
+        self.kind = kind
+        self.color = color
+        self.shape = shape
